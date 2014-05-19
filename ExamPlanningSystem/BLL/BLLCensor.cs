94d8466f9aeb78372438d7a851ad61e7d5e0236e
Kallaen <kallecs@hotmail.com>
2014-05-19 10:27:46 +0200

    ...

---
+++ ExamPlanningSystem/BLL/BLLCensor.cs
@@ -0,0 +1,64 @@
+﻿using DALC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BLL
+{
+    public class BLLCensor
+    {
+        private ExamPlanningSystemSDMEntities1 entities;
+        private ICensorContext fakeContext;
+        private bool _realDBAccess;
+
+        public BLLCensor(bool realDBAccess = true)
+        {
+            entities = new ExamPlanningSystemSDMEntities1();
+            fakeContext = new FakeCensorContext();
+            _realDBAccess = realDBAccess;
+        }
+
+        public void addCensor(Censor _censor)
+        {
+            if (_realDBAccess)
+                addCensorReal(_censor);
+            else
+                addCensorFake(_censor);
+        }
+
+        public Censor getCensor(int censorID)
+        {
+            if (_realDBAccess)
+                return getCensorReal(censorID);
+            else
+                return getCensorFake(censorID);
+        }
+
+
+
+        private void addCensorReal(Censor _censor)
+        {
+            entities.Censor.Add(_censor);
+            entities.SaveChanges();
+        }
+
+        private void addCensorFake(Censor _censor)
+        {
+            fakeContext.Censor.Add(_censor);
+            fakeContext.SaveChanges();
+        }
+
+        private Censor getCensorReal(int censorID)
+        {
+            return entities.Censor.Single(c => c.Id == censorID);
+        }
+
+        private Censor getCensorFake(int censorID)
+        {
+            return fakeContext.Censor.Single(c => c.Id == censorID);
+        }
+    }
+}
---
+++ ExamPlanningSystem/DALC/FakeBEContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using System.Data.Entity;
+
+namespace DALC
+{
+    #region ClassContext
+    public class FakeClassContext : IClassContext
+    {
+        public FakeClassContext()
+        {
+            this.Class = new FakeClassContextSet();
+        }
+
+        public IDbSet<Class> Class
+        {
+            get;
+            private set;
+        }
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+    }
+
+    public class FakeClassContextSet : FakeDbSet<Class>
+    {
+        public override Class Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(d => d.ClassID == (int)keyValues.Single());
+        }
+    }
+    #endregion
+
+    #region TeacherContext
+    public class FakeTeacherContext : ITeacherContext
+    {
+        public FakeTeacherContext()
+        {
+            this.Teacher = new FakeTeacherContextSet();
+        }
+
+        public IDbSet<Teachers> Teacher
+        {
+            get;
+            private set;
+        }
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+    }
+
+    public class FakeTeacherContextSet : FakeDbSet<Teachers>
+    {
+        public override Teachers Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(d => d.Id == (int)keyValues.Single());
+        }
+    }
+    #endregion
+
+    #region CensorContext
+    public class FakeCensorContext : ICensorContext
+    {
+        public FakeCensorContext()
+        {
+            this.Censor = new FakeCensorContextSet();
+        }
+
+        public IDbSet<Censor> Censor
+        {
+            get;
+            private set;
+        }
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+    }
+
+    public class FakeCensorContextSet : FakeDbSet<Censor>
+    {
+        public override Censor Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(d => d.Id == (int)keyValues.Single());
+        }
+    }
+    #endregion
+}
